63d728c16ea31af7c06cdee9f57b832a43bc70be
Alister Lewis-Bowen <alister@lewis-bowen.org>
2021-11-23 15:25:57 -0500

    Add averages

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Inspired by [this article](https://www.raspberrypi.com/news/monitor-air-quality-with-a-raspberry-pi/), I wanted to monitor the particulate matter I was breathing in my garage workshop.
 
+Adafruit-IO API:
+https://adafruit-io-python-client.readthedocs.io/en/latest/data.html 
+
+SDS011 sesnor datasheet:
+https://microcontrollerslab.com/wp-content/uploads/2020/12/NonA-PM-SDS011-Dust-sensor-datasheet.pdf
+
 [Acceptable levels for my area](https://www3.epa.gov/region1/airquality/pm-aq-standards.html).
 For PM10: 150 microns per cubic meters based on a 24-hour average
 For PM2.5: 35 microns per cubin meters based on a 24-hour average
--- monitor.py
+++ monitor.py
@@ -3,19 +3,28 @@
 # Pull data from SDS011 sensor and push it to an AdaFruit IO dashboard
 # @author Alister Lewis-Bowen <alister@lewis-bowen.org>
 
-import os, serial, time
+import os
+import time
+
+import serial
 from Adafruit_IO import Client
 
 DEVICE='/dev/ttyUSB0'
+SAMPLE_TIME=60
+SAMPLES_DAY=(60*60*24)/SAMPLE_TIME
+SAMPLES_YEAR=(60*60*24*356)/SAMPLE_TIME
 
-# Adafruit-IO API:
-# https://adafruit-io-python-client.readthedocs.io/en/latest/data.html 
 AIO_USER='ali5ter'
 AIO_KEY_FILE=os.environ['HOME'] + '/.config/adafruit-io-key'
 with open(AIO_KEY_FILE, 'r') as file:
     AIO_KEY = file.read().replace('\n', '')
-AIO_FEED_PM_SMALL_NAME='garage-pm-2-dot-5'
-AIO_FEED_PM_LARGE_NAME='garage-pm-10'
+
+AIO_FEED_PM_SMALL_NAME='pm-2-dot-5'
+AIO_FEED_PM_SMALL_ROLLING_MEAN='pm-2-dot-5-rolling-mean'
+AIO_FEED_PM_SMALL_DAILY_MEAN='pm-2-dot-5-daily-mean'
+AIO_FEED_PM_LARGE_NAME='pm-10'
+AIO_FEED_PM_LARGE_ROLLING_MEAN='pm-10-rolling-mean'
+AIO_FEED_PM_LARGE_DAILY_MEAN='pm-10-daily-mean'
 
 ser = serial.Serial(DEVICE)
 aio = Client(AIO_USER, AIO_KEY)
@@ -26,24 +35,47 @@ aio = Client(AIO_USER, AIO_KEY)
 #     print('Feed: {0}'.format(f.name))
 
 pm_small_feed = aio.feeds(AIO_FEED_PM_SMALL_NAME)
+pm_small_feed_rolling = aio.feeds(AIO_FEED_PM_SMALL_ROLLING_MEAN)
+pm_small_feed_daily = aio.feeds(AIO_FEED_PM_SMALL_DAILY_MEAN)
 pm_large_feed = aio.feeds(AIO_FEED_PM_LARGE_NAME)
+pm_large_feed_rolling = aio.feeds(AIO_FEED_PM_LARGE_ROLLING_MEAN)
+pm_large_feed_daily = aio.feeds(AIO_FEED_PM_LARGE_DAILY_MEAN)
+
+# TODO: Fetch any stored averages
+sample=0
+pm_small_total=0
+pm_large_total=0
+pm_small_mean=0
+pm_large_mean=0
 
 while True:
 
     data = []
-    # SDS011 sesnor datasheet:
-    # https://microcontrollerslab.com/wp-content/uploads/2020/12/NonA-PM-SDS011-Dust-sensor-datasheet.pdf
-
     for index in range(0,10):
         datum = ser.read()
         data.append(datum)
-
+        
+    sample += 1
     pm_small = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
     pm_large = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
 
-    print(f"Data point: pm25 = {pm_small}  pm10 = {pm_large}")
-
+    print(f"{sample}: {SAMPLE_TIME}sSample pm25 = {pm_small}  pm10 = {pm_large}")
     aio.send_data(pm_small_feed.key, pm_small)
     aio.send_data(pm_large_feed.key, pm_large)
 
-    time.sleep(10)
+    pm_small_total += pm_small
+    pm_large_total += pm_large
+    pm_small_mean = pm_small_total/sample
+    pm_large_mean = pm_large_total/sample
+    print(f"{sample}: RollingMean pm25 = {pm_small_mean}  pm10 = {pm_large_mean}")
+    aio.send_data(pm_small_feed_rolling.key, pm_small_mean)
+    aio.send_data(pm_large_feed_rolling.key, pm_large_mean)
+
+    if sample == (60*60*24)/SAMPLE_TIME:
+        print(f"{sample}: 24hMean pm25 = {pm_small_mean}  pm10 = {pm_large_mean}")
+        aio.send_data(pm_small_feed_daily.key, pm_small_mean)
+        aio.send_data(pm_large_feed_daily.key, pm_large_mean)
+        pm_small_total = 0
+        pm_large_total = 0
+
+    time.sleep(SAMPLE_TIME)
