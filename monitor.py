e6c612a87e8537a01dc86a44c9dda9b05f4865a5
Alister Lewis-Bowen <alister@lewis-bowen.org>
2021-12-27 09:57:31 -0500

    fix rolling ave and add feed creation

--- monitor.py
+++ monitor.py
@@ -6,6 +6,7 @@
 
 import os
 import time
+import argparse
 import requests
 import json
 import serial
@@ -13,7 +14,14 @@ import board
 import digitalio
 import adafruit_bme680
 from urllib.error import HTTPError
-from Adafruit_IO import Client
+from Adafruit_IO import Client, Feed, Group
+
+# Set up input args
+all_args = argparse.ArgumentParser(
+    description='Monitor sensors connected to RPi measuring environmental conditions'
+)
+all_args.add_argument('-I', '--init', action="store_true", default=False, help='Initialize the Adafruit IO feeds')
+args = vars(all_args.parse_args())
 
 # Monitor configuration
 LOOP_DELAY = 5
@@ -22,8 +30,6 @@ SAMPLES_DAY = ((60*60*24)/SAMPLE_TIME)/LOOP_DELAY
 SERIAL_DEVICE = '/dev/ttyUSB0'
 BME680_TEMP_OFFSET = -5
 PIR_PIN = board.D4
-AW_CALL_LIMIT = 50 # Free licence limited to 50 API calls per day
-AW_SAMPLE_TIME = ((60*60*24)/AW_CALL_LIMIT)/LOOP_DELAY
 
 # Accuweather configuration
 AW_URL_BASE = 'http://dataservice.accuweather.com'
@@ -33,33 +39,41 @@ with open(AW_KEY_FILE, 'r') as file:
     AW_KEY = file.read().replace('\n', '')
 # Found using /locations/v1/cities/search Accuweather API call
 AW_LOCATION_KEY = '329319'    # Cambridge, MA
+AW_CALL_LIMIT = 50 # Free licence limited to 50 API calls per day
+AW_SAMPLE_TIME = ((60*60*24)/AW_CALL_LIMIT)/LOOP_DELAY
 
 # Adafruit configuration
 AIO_USER = 'ali5ter'
 AIO_KEY_FILE = os.environ['HOME'] + '/.config/adafruit-io-key'
 with open(AIO_KEY_FILE, 'r') as file:
     AIO_KEY = file.read().replace('\n', '')
+AIO_RECREATE_FEEDS = args['init']
 AIO_GROUP = 'garage-env'
-AIO_FEED_GAS = AIO_GROUP+'.gas'
-AIO_FEED_HUMIDITY = AIO_GROUP+'.humidity'
-AIO_FEED_HUMIDITY_ROLLING_AVE = AIO_GROUP+'.humidity-rolling-ave'
-AIO_FEED_HUMIDITY_LOCAL = AIO_GROUP+'.local-humidity'
-AIO_FEED_PM_LARGE = AIO_GROUP+'.pm10'
-AIO_FEED_PM_LARGE_ROLLING_AVE = AIO_GROUP+'.pm10-rolling-ave'
-AIO_FEED_PM_LARGE_24H_AVE = AIO_GROUP+'.pm10-24h-ave'
-AIO_FEED_PM_SMALL = AIO_GROUP+'.pm2-dot-5'
-AIO_FEED_PM_SMALL_ROLLING_AVE = AIO_GROUP+'.pm2-dot-5-rolling-ave'
-AIO_FEED_PM_SMALL_24H_AVE = AIO_GROUP+'.pm2-dot-5-24h-ave'
-AIO_FEED_PRESSURE = AIO_GROUP+'.pressure'
-AIO_FEED_PRESSURE_ROLLING_AVE = AIO_GROUP+'.pressure-rolling-ave'
-AIO_FEED_PRESSURE_LOCAL = AIO_GROUP+'.local-pressure'
-AIO_FEED_TEMP = AIO_GROUP+'.temp'
-AIO_FEED_TEMP_ROLLING_AVE = AIO_GROUP+'.temp-rolling-ave'
-AIO_FEED_TEMP_LOCAL = AIO_GROUP+'.local-temperature'
-AIO_FEED_MOTION = AIO_GROUP+'.motion'
+AIO_FEED_NAMES = [
+    'pm10',
+    'pm10 ave',
+    'pm10 24h ave',
+    'pm2.5',
+    'pm2.5 ave',
+    'pm2.5 24h ave',
+    'temperature',
+    'temperature ave',
+    'temperature local',
+    'pressure',
+    'pressure ave',
+    'pressure local',
+    'humidity',
+    'humidity ave',
+    'humidity local',
+    'gas',
+    'motion'
+]
+AIO_FEEDS={}
 
 # TODO: Fetch any stored averages
-sample = 0
+loop = 0
+sample_sds011 = 0
+sample_bme860 = 0
 c_tempC = 0
 c_tempF = 0
 c_humidity = 0
@@ -86,41 +100,41 @@ pir_old_value = pir_current_value
 # Connection to AIO
 aio = Client(AIO_USER, AIO_KEY)
 
-# Access AIO feeds
-f_gas = aio.feeds(AIO_FEED_GAS)
-f_humidity = aio.feeds(AIO_FEED_HUMIDITY)
-f_humidity_rolling = aio.feeds(AIO_FEED_HUMIDITY_ROLLING_AVE)
-f_humidity_local = aio.feeds(AIO_FEED_HUMIDITY_LOCAL)
-f_pm_large = aio.feeds(AIO_FEED_PM_LARGE)
-f_pm_large_rolling = aio.feeds(AIO_FEED_PM_LARGE_ROLLING_AVE)
-f_pm_large_24h = aio.feeds(AIO_FEED_PM_LARGE_24H_AVE)
-f_pm_small = aio.feeds(AIO_FEED_PM_SMALL)
-f_pm_small_rolling = aio.feeds(AIO_FEED_PM_SMALL_ROLLING_AVE)
-f_pm_small_24h = aio.feeds(AIO_FEED_PM_SMALL_24H_AVE)
-f_pressure = aio.feeds(AIO_FEED_PRESSURE)
-f_pressure_rolling = aio.feeds(AIO_FEED_PRESSURE_ROLLING_AVE)
-f_pressure_local = aio.feeds(AIO_FEED_PRESSURE_LOCAL)
-f_temp = aio.feeds(AIO_FEED_TEMP)
-f_temp_rolling = aio.feeds(AIO_FEED_TEMP_ROLLING_AVE)
-f_temp_local = aio.feeds(AIO_FEED_TEMP_LOCAL)
-f_motion = aio.feeds(AIO_FEED_MOTION)
+# Connect AIO Feeds
+if AIO_RECREATE_FEEDS:
+    try:
+        aio.delete_group(AIO_GROUP)
+    except Exception as err:
+        print(f'AIO error: {err}')
+    group = Group(name=AIO_GROUP)
+    group = aio.create_group(group)
+    for name in AIO_FEED_NAMES:
+        print(f"Creating feed for {name}")
+        feed = Feed(name=name)
+        AIO_FEEDS[name] = aio.create_feed(feed, group_key=AIO_GROUP)
+    
+else:
+    group = aio.groups(AIO_GROUP)
+    for feed in group.feeds:
+        print(f"Connecting feed for {feed.name}")
+        AIO_FEEDS[feed.name] = aio.feeds(feed.key)
 
 while True:
 
-    sample += 1
+    loop += 1
     ts = time.time()
 
     # Fetch current weather conditions ---------------------------------------
 
-    if (sample-1) % AW_SAMPLE_TIME == 0:
+    if (loop-1) % AW_SAMPLE_TIME == 0:
         try:
-            print(f"{sample}@{ts}: Fetching current weather conditions")
+            print(f"{loop}@{ts}: Fetching current weather conditions")
             r = requests.get(AW_URL_CONDITIONS.format(AW_LOCATION_KEY, AW_KEY))
             r.raise_for_status()
         except HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}')
+            print(f'HTTP error: {http_err}')
         except Exception as err:
-            print(f'Other error occurred: {err}')
+            print(f'Other error: {err}')
         else:
             json_data = json.loads(r.text)
             for data in json_data:
@@ -134,14 +148,15 @@ while True:
         print("\t Current pressure: %0.3f hPa (mb)" % c_pressure)
 
         # Write current weather data to AIO feeds
-        aio.send_data(f_humidity_local.key, c_humidity)
-        aio.send_data(f_pressure_local.key, c_pressure)
-        aio.send_data(f_temp_local.key, c_tempF)
+        aio.send_data(AIO_FEEDS['humidity local'].key, c_humidity)
+        aio.send_data(AIO_FEEDS['pressure local'].key, c_pressure)
+        aio.send_data(AIO_FEEDS['temperature local'].key, c_tempF)
 
     # Fetch SDS011 data ------------------------------------------------------
 
-    if sample % SAMPLE_TIME == 0:
-        print(f"{sample}@{ts}: Fetching SDS011 sensor data")
+    if loop % SAMPLE_TIME == 0:
+        sample_sds011 += 1
+        print(f"{loop}@{ts}: Fetching SDS011 sensor data")
 
         data = []
         for index in range(0,10):
@@ -154,26 +169,27 @@ while True:
         # Calc rolling averages for SDS011 data
         total_pm_small += pm_small
         total_pm_large += pm_large
-        ave_pm_small = total_pm_small/sample
-        ave_pm_large = total_pm_large/sample
+        ave_pm_small = total_pm_small/sample_sds011
+        ave_pm_large = total_pm_large/sample_sds011
         print(f"\t PM2.5 ave = {ave_pm_small}  PM10 ave = {ave_pm_large}")
 
         # Write SDS011 data to AIO feeds
-        aio.send_data(f_pm_small.key, pm_small)
-        aio.send_data(f_pm_large.key, pm_large)
-        aio.send_data(f_pm_small_rolling.key, ave_pm_small)
-        aio.send_data(f_pm_large_rolling.key, ave_pm_large)
+        aio.send_data(AIO_FEEDS['pm2.5'].key, pm_small)
+        aio.send_data(AIO_FEEDS['pm10'].key, pm_large)
+        aio.send_data(AIO_FEEDS['pm2.5 ave'].key, ave_pm_small)
+        aio.send_data(AIO_FEEDS['pm10 ave'].key, ave_pm_large)
 
         # Write 24h SDS011 data to AIO feeds
-        if (sample-1) % SAMPLES_DAY == 0:
-            aio.send_data(f_pm_small_24h.key, ave_pm_small)
-            aio.send_data(f_pm_large_24h.key, ave_pm_large)
+        if (loop-1) % SAMPLES_DAY == 0:
+            aio.send_data(AIO_FEEDS['pm2.4 24h ave'].key, ave_pm_small)
+            aio.send_data(AIO_FEEDS['pm10 24h ave'].key, ave_pm_large)
             print(f"\t PM2.5 24h ave = {ave_pm_small}  PM10 24h ave = {ave_pm_large}")
 
     # Fetch BME680 data ------------------------------------------------------
 
-    if sample % SAMPLE_TIME == 0:
-        print(f"{sample}@{ts}: Fetching BME680 sensor data")
+    if loop % SAMPLE_TIME == 0:
+        sample_bme860 += 1
+        print(f"{loop}@{ts}: Fetching BME680 sensor data")
 
         bme680.sea_level_pressure = c_pressure    # Calibrate BME680
         tempC = bme680.temperature + BME680_TEMP_OFFSET
@@ -192,40 +208,36 @@ while True:
         total_humidity += humidity
         total_pressure += pressure
         total_temp += tempF
-        ave_humidity = total_humidity/sample
-        ave_pressure = total_pressure/sample
-        ave_temp = total_temp/sample
+        ave_humidity = total_humidity/sample_bme860
+        ave_pressure = total_pressure/sample_bme860
+        ave_temp = total_temp/sample_bme860
         print(f"\t Humidity ave = {ave_humidity}  Pressure ave = {ave_pressure}  Temperature ave = {ave_temp}")
 
         # Write BME680 data to AIO feeds
-        aio.send_data(f_temp.key, tempF)
-        aio.send_data(f_gas.key, gas)
-        aio.send_data(f_humidity.key, humidity)
-        aio.send_data(f_pressure.key, pressure)
-        aio.send_data(f_humidity_rolling.key, ave_humidity)
-        aio.send_data(f_pressure_rolling.key, ave_pressure)
-        aio.send(f_temp_rolling.key, ave_temp)
+        aio.send_data(AIO_FEEDS['temperature'].key, tempF)
+        aio.send_data(AIO_FEEDS['temperature ave'].key, ave_temp)
+        aio.send_data(AIO_FEEDS['gas'].key, gas)
+        aio.send_data(AIO_FEEDS['humidity'].key, humidity)
+        aio.send_data(AIO_FEEDS['humidity ave'].key, ave_humidity)
+        aio.send_data(AIO_FEEDS['pressure'].key, pressure)
+        aio.send_data(AIO_FEEDS['pressure ave'].key, ave_pressure)
 
     # Fetch PIR data ---------------------------------------------------------
 
-    print(f"{sample}@{ts}: Fetching PIR sensor data")
+    print(f"{loop}@{ts}: Fetching PIR sensor data")
 
     pir_current_value = pir.value
     if pir_current_value:
         if not pir_old_value:
             motion = 1
             print("\t Motion detected")
-        aio.send_data(f_motion.key, 1)
+        aio.send_data(AIO_FEEDS['motion'].key, 1)
     else:
         if pir_old_value:
             motion = 0
             print("\t Motion ended")
-        aio.send_data(f_motion.key, 0)
+        aio.send_data(AIO_FEEDS['motion'].key, 0)
     pir_old_value = pir_current_value
 
-
-    # Reset sample counter after 24h 
-    if sample % SAMPLES_DAY == 0:
-        sample = 0
 
     time.sleep(LOOP_DELAY)
