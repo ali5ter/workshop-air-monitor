54d1fe14eb925eeb35b6f50e698e1bd1b842d0b8
Alister Lewis-Bowen <alister@lewis-bowen.org>
2025-07-24 13:58:02 -0400

    fix: add logging for PIR sensor GPIO pin initialization

--- client/env_monitor/pir.py
+++ client/env_monitor/pir.py
@@ -12,6 +12,7 @@ class PIR(object):
 
         # THE GPIO pin to use as the PIR sensor digital input
         self.input_pin = pir_sensor_gpio_pin
+        logging.debug(f"PIR sensor GPIO pin set: {self.input_pin}")
 
         # The number of loops after which to fetch sensor data
         self.sample_time = sample_time
