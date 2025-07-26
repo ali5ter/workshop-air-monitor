a5417bb55d03533d878beb3566e6d8d27aed821b
Alister Lewis-Bowen <alister@lewis-bowen.org>
2025-07-26 15:23:23 -0400

    fix: refactor OpenWeather API key handling to use instance variable

--- client/env_monitor/openweather.py
+++ client/env_monitor/openweather.py
@@ -16,20 +16,20 @@ class OpenWeather(object):
         # OpenWeather API base URL
         self.url_base = 'http://api.openweathermap.org/data/2.5/weather'
 
-        # OpenWeather location used to fetch current conditions (city name or lat/lon tuple)
-        if isinstance(location, tuple):
-            lat, lon = location
-            query = (f"?lat={lat}&lon={lon}&appid={openweather_api_key}&units=metric")
-        else:
-            query = (f"?q={location}&appid={openweather_api_key}&units=metric")
-
-        # OpenWeather API method to fetch current weather conditions
-        self.conditions_method = self.url_base + query
-
         # File containing the OpenWeather API access key
         self.key_file = openweather_api_key
         with open(self.key_file, 'r') as file:
             self.key = file.read().replace('\n', '')
+
+        # OpenWeather location used to fetch current conditions (city name or lat/lon tuple)
+        if isinstance(location, tuple):
+            lat, lon = location
+            query = (f"?lat={lat}&lon={lon}&appid={self.key}&units=metric")
+        else:
+            query = (f"?q={location}&appid={self.key}&units=metric")
+
+        # OpenWeather API method to fetch current weather conditions
+        self.conditions_method = self.url_base + query
 
         # The number of loops after which to fetch sensor data
         self.sample_time = sample_time
